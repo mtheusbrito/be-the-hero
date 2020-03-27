c5139c8c018354517c645732f6d938265d28e5e7
Matheus Brito <matheusbritodasilva@gmail.com>
2020-03-27 01:53:18 -0300

    logon page

---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default function Header({children}){
+    return (
+        <header>
+            <h1>{children}</h1>
+        </header>
+    );
+}
+
