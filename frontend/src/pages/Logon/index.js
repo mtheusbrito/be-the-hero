7b0cb3dbb49e5ffb2e74b04a694a72d068b2a7a1
Matheus Brito <matheusbritodasilva@gmail.com>
2020-03-28 18:06:38 -0300

    logging in

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,51 @@
-import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { FiLogIn } from "react-icons/fi";
+import { Link , useHistory} from "react-router-dom";
 
-import './styles.css';
-import logoImg from '../../assets/logo.svg';
-import herosImg from '../../assets/heroes.png';
+import "./styles.css";
+import logoImg from "../../assets/logo.svg";
+import herosImg from "../../assets/heroes.png";
+import api from "../../services/api";
+
 export default function Logon() {
+  const [id, setId] = useState("");
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+ 
+    try {
+      const response = await api.post("sessions", { id });
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+      
+
+    } catch (err) {
+      alert('Falha no login, tente novamente!');
+
+    }
+  }
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
-          <input type="text" placeholder="Sua ID" />
-          <button  className="button" type="submit">Entrar</button>
+          <input
+            type="text"
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+          <button className="button" type="submit">
+            Entrar
+          </button>
 
-          <Link to="/register" className="back-link"><FiLogIn size={16} color="#E02041" />Não tenho cadastro</Link>
+          <Link to="/register" className="back-link">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </Link>
         </form>
       </section>
       <img src={herosImg} alt="Heroes" />
